d81029ba9195719e6ef6c3ad9115724073d831dd
Eklavyakumar2002 <122663535+Eklavyakumar2002@users.noreply.github.com>
2026-06-11 23:40:36 +0530

    Update ExtentManager.java

--- GUI Elements Automation Framework/src/test/java/utilities/ExtentManager.java
+++ GUI Elements Automation Framework/src/test/java/utilities/ExtentManager.java
@@ -3,7 +3,6 @@ package utilities;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
-import com.aventstack.extentreports.reporter.configuration.Theme;
 
 public class ExtentManager {
 
@@ -17,22 +16,16 @@ public class ExtentManager {
             ExtentSparkReporter sparkReporter =
                     new ExtentSparkReporter("test-output/ExtentReport.html");
 
-            // Report Configuration
             sparkReporter.config().setReportName("GUI Automation Report");
             sparkReporter.config().setDocumentTitle("Selenium Capstone Project");
-            sparkReporter.config().setTheme(Theme.STANDARD);
-            sparkReporter.config().setTimeStampFormat("dd-MM-yyyy HH:mm:ss");
 
             extent = new ExtentReports();
             extent.attachReporter(sparkReporter);
 
-            // System Information
             extent.setSystemInfo("Tester", "Eklavya Kumar");
             extent.setSystemInfo("Framework", "Selenium + TestNG");
             extent.setSystemInfo("Project", "GUI Automation");
             extent.setSystemInfo("Environment", "QA");
-            extent.setSystemInfo("OS", System.getProperty("os.name"));
-            extent.setSystemInfo("Java Version", System.getProperty("java.version"));
         }
 
         return extent;
